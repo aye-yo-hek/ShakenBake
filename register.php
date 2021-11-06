4472a81117c2573a1f36896390ed0cb9ae2e9fb3
Hector Hernandez <84157981+aye-yo-hek@users.noreply.github.com>
2021-11-06 13:13:44 -0700

    Create register.php

---
+++ register.php
@@ -0,0 +1,57 @@
+<html lang="en">
+
+<head>
+
+  <meta charset="UTF-8">
+
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <title>Register</title>
+
+</head>
+
+<body>
+
+  <center>
+
+  <h3>Register Here</h3>
+
+  <form action="" method="post">
+
+      <table>
+
+        <tr>
+
+          <td>Username: 	player1</td>
+
+        </tr>
+
+        <tr>
+
+          <td>Password:</td> <!-- ex. +419939c0b5a+ -->
+
+          <td><input type="password" name="user_pass" placeholder=" "></td>
+
+        </tr>
+
+        <tr>
+
+           <td><input type="submit" name="submit" value="submit"></td>
+
+           <td><p> Already a user? <a href="login.php">Login Here</a></p></td>
+
+        </tr>
+
+      </table>
+
+  </form>
+
+  
+
+  </center>
+
+</body>
+
+</html>
