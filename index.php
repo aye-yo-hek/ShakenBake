007fc4cc3ad9d6f04fcb47cbc5cf4e75da64d146
Hector Hernandez <84157981+aye-yo-hek@users.noreply.github.com>
2021-11-06 13:12:22 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,61 @@
+<html lang="en">
+
+<head>
+
+  <meta charset="UTF-8">
+
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <title></title>
+
+</head>
+
+<body>
+
+  <center>
+
+  <h3>Login Here</h3>
+
+  <form action="" method="post">
+
+      <table>
+
+        <tr>
+
+          <td>Username:</td>
+
+          <td><input type="text" name="user" placeholder="ex. 2cb198c22046d+ "></td>
+
+        </tr>
+
+        <tr>
+
+          <td>Password:</td>
+
+          <td><input type="password" name="user_pass" placeholder="ex. +775213ed "></td>
+
+        </tr>
+
+        
+
+        <tr>
+
+           <td><input type="submit" name="submit" value="Login"></td>
+
+           <td><p>Not yet a Memeber? <a href="register.php">Register</a></p></td>
+
+        </tr>
+
+      </table>
+
+  </form>
+
+  <!-- -->
+
+  </center>
+
+</body>
+
+</html>
